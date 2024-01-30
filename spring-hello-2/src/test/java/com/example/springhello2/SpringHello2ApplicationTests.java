ddb2cf1526d34301277992467e8e83537dd73555
ilaydadastan <ilayda@alpha3d.io>
2024-01-31 01:07:13 +0200

    added CommandLineRunner

---
+++ spring-hello-2/src/test/java/com/example/springhello2/SpringHello2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhello2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHello2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
